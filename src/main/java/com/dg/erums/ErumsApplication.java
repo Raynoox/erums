87c869b3c1e5eb476ee63ee6a68b51fa1ee7a283
DamianG <damian.grzeszczuk@gmail.com>
2017-08-03 21:46:18 +0200

    setup new gradle project

---
+++ src/main/java/com/dg/erums/ErumsApplication.java
@@ -0,0 +1,12 @@
+package com.dg.erums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErumsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErumsApplication.class, args);
+	}
+}
